ef50085747c9f13ea7c7ab07178fda94cb1f73d8
Michael Lewallen <lewallen.d@live.com>
2016-10-28 14:22:28 -0500

    Add project files.

---
+++ CSCE4444-WPF-Pizza/OrderWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaBlendGrid {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class OrderWindow : Window {
+        public OrderWindow() {
+            InitializeComponent();
+        }
+
+        private void Show_Click(object sender, RoutedEventArgs e) {
+            MyPopup.IsOpen = true;
+        }
+
+        private void Hide_Click(object sender, RoutedEventArgs e) {
+            MyPopup.IsOpen = false;
+        }
+    }
+}
