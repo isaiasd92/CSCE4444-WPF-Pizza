e1f0c8c99fd5d55234656f92a0e1338897f7727c
Michael Lewallen <lewallen.d@live.com>
2016-10-29 07:46:32 -0500

    updates to qty button calc, build not broken, need to fix double click issue with calc buttons in popup

--- CSCE4444-WPF-Pizza/OrderWindow.xaml.cs
+++ CSCE4444-WPF-Pizza/OrderWindow.xaml.cs
@@ -18,8 +18,16 @@ namespace PizzaBlendGrid {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class OrderWindow : Window {
+
+        // base state
+        string qtyPizzaButton_display = "1";
+        bool qtyPizzaButton_changed = false;
+
         public OrderWindow() {
             InitializeComponent();
+
+            //clean this up
+            qtyPizzaButton.Content = "1";
         }
 
         private void Show_Click(object sender, RoutedEventArgs e) {
@@ -29,5 +37,52 @@ namespace PizzaBlendGrid {
         private void Hide_Click(object sender, RoutedEventArgs e) {
             MyPopup.IsOpen = false;
         }
+
+        private void Click_Digit1(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("1");
+        }
+
+        private void Click_Digit2(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("2");
+        }
+        private void Click_Digit3(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("3");
+        }
+        private void Click_Digit4(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("4");
+        }
+        private void Click_Digit5(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("5");
+        }
+        private void Click_Digit6(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("6");
+        }
+        private void Click_Digit7(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("7");
+        }
+        private void Click_Digit8(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("8");
+        }
+        private void Click_Digit9(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("9");
+        }
+
+        private void Click_Digit0(object sender, RoutedEventArgs e) {
+            QtyPizzaButton_update("0");
+        }
+
+        private void QtyPizzaButton_update(string numinput) {
+            if (!qtyPizzaButton_changed) {
+                qtyPizzaButton.Content = numinput;
+                qtyPizzaButton_display = numinput;
+                qtyPizzaButton_changed = true;
+            } else {
+                qtyPizzaButton_display = qtyPizzaButton_display + numinput;
+                qtyPizzaButton.Content = qtyPizzaButton_display;
+            }
+        }
     }
+
+    
+
 }
